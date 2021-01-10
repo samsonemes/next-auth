401df2552bfc80d10db59e7f6fc8c9a1de4d0cb8
Samson Monyluak <50766123+samsonemes@users.noreply.github.com>
2021-01-10 16:45:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        mongoURI: "mongodb+srv://dbNext:dbNext123@nextauthcluster.lm8jt.mongodb.net/<dbname>?retryWrites=true&w=majority",
+        mongoURI: "mongodb+srv://dbNext:<password>@nextauthcluster.lm8jt.mongodb.net/<dbname>?retryWrites=true&w=majority",
         ACCESS_SECRET: "e93836a5-122b-486d-bd42-d25cb7201412",
     }
 }
